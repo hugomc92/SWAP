39d67d413a9083822227e3518ab8c79d533f4e66
Hugo Maldonado <hugomc92@gmail.com>
2017-05-29 11:35:45 +0200

    Trabajo y fixes

--- Prácticas/Práctica4/scripts/iptables.sh
+++ Prácticas/Práctica4/scripts/iptables.sh
@@ -1,5 +1,3 @@
-#!/bin/sh
-
 # Borrar todas las configuraciones para empezar desde un estado limpio
 iptables -F
 iptables -t nat -F
@@ -9,7 +7,6 @@ iptables -t nat -X
 # Bloquear todo el tráfico
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
 
 # Permitir cualquier acceso desde localhost
 iptables -A INPUT -i lo -j ACCEPT
@@ -18,6 +15,10 @@ iptables -A OUTPUT -o lo -j ACCEPT
 # Permitir la conexión por ssh en el puerto 22
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
+
+# Permitir las conexiones HTTP/HTTPS (80 y 443)
+iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
 
 # Permitir el redireccionamiento entre máquinas
 echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -29,7 +30,3 @@ iptables -t nat -A POSTROUTING -p tcp -d 10.211.55.11 --dport 80 -j SNAT --to-so
 # Redireccionamiento HTTPS
 iptables -t nat -A PREROUTING -p tcp --dport 443 -j DNAT --to-destination 10.211.55.11:443
 iptables -t nat -A POSTROUTING -p tcp -d 10.211.55.11 --dport 443 -j SNAT --to-source 10.211.55.12
-
-# Permitir las conexiones HTTP/HTTPS (80 y 443)
-iptables -A INPUT -m state --state NEW -p tcp --dport 80 -j ACCEPT
-iptables -A INPUT -m state --state NEW -p tcp --dport 443 -j ACCEPT
